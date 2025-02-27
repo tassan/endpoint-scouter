95db8d2eb4da510ce0fcbb32ea48f330e607922f
Flávio Tassan <ftassan@outlook.com>
2025-02-26 23:31:13 -0300

    feat: Add utilities package for EndpointScouter with configuration loader

---
+++ endpoint_scouter/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utilities package for EndpointScouter.
+
+This package contains utility modules for the scanner.
+"""
+
+from endpoint_scouter.utils.config import load_config
+
+__all__ = ['load_config']
